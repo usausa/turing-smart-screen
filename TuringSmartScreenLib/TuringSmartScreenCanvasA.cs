259cfc95e8c66cbb78925901f8a75a1fc0b82829
usausa <machi.pon@gmail.com>
2022-10-22 18:05:58 +0900

    Update canvas

--- TuringSmartScreenLib/TuringSmartScreenCanvasA.cs
+++ TuringSmartScreenLib/TuringSmartScreenCanvasA.cs
@@ -1,6 +1,42 @@
 namespace TuringSmartScreenLib;
 
-public class TuringSmartScreenCanvasA
+using System.Runtime.CompilerServices;
+
+public sealed class TuringSmartScreenCanvasA
 {
-    // TODO
+    private readonly TuringSmartScreenRevisionA screen;
+
+    private readonly int top;
+
+    private readonly int left;
+
+    private readonly int width;
+
+    private readonly int height;
+
+    private readonly byte[] buffer;
+
+    public TuringSmartScreenCanvasA(TuringSmartScreenRevisionA screen, int top, int left, int width, int height)
+    {
+        this.screen = screen;
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+        buffer = new byte[width * height * 2];
+    }
+
+    public void Write()
+    {
+        screen.DisplayBitmap(top, left, width, height, buffer);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void SetPixel(int x, int y, byte r, byte g, byte b)
+    {
+        var rgb = ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3);
+        var offset = ((y * width) + x) * 2;
+        buffer[offset] = (byte)(rgb & 0xFF);
+        buffer[offset + 1] = (byte)((rgb >> 8) & 0xFF);
+    }
 }
