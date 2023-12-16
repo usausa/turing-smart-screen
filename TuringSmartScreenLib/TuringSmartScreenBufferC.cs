47dd882e7bf04c410abb20f38f64960624aa591a
usausa <machi.pon@gmail.com>
2023-12-16 15:02:05 +0900

    Update

--- TuringSmartScreenLib/TuringSmartScreenBufferC.cs
+++ TuringSmartScreenLib/TuringSmartScreenBufferC.cs
@@ -1,39 +1,39 @@
-//namespace TuringSmartScreenLib;
+namespace TuringSmartScreenLib;
 
-//using System;
+using System;
 
-//#pragma warning disable CA1819
-//#pragma warning disable IDE0032
-//public sealed class TuringSmartScreenBufferC : IScreenBuffer
-//{
-//    internal byte[] ImgBuffer { get; set; } = Array.Empty<byte>();
+#pragma warning disable CA1819
+#pragma warning disable IDE0032
+public sealed class TuringSmartScreenBufferC : IScreenBuffer
+{
+    internal byte[] ImgBuffer { get; set; } = Array.Empty<byte>();
 
-//    public int Width { get; private set; }
+    public int Width { get; private set; }
 
-//    public int Height { get; private set; }
+    public int Height { get; private set; }
 
-//    public int Length => ImgBuffer.Length;
+    public int Length => ImgBuffer.Length;
 
-//    public void SetPixel(int x, int y, byte r, byte g, byte b)
-//    {
-//        ImgBuffer[(y * Width) + x] = r;
-//        ImgBuffer[(y * Width) + x + 1] = g;
-//        ImgBuffer[(y * Width) + x + 2] = b;
-//    }
+    public void SetPixel(int x, int y, byte r, byte g, byte b)
+    {
+        ImgBuffer[(y * Width) + x] = r;
+        ImgBuffer[(y * Width) + x + 1] = g;
+        ImgBuffer[(y * Width) + x + 2] = b;
+    }
 
-//    public void Clear(byte r = 0, byte g = 0, byte b = 0) => ImgBuffer = Array.Empty<byte>();
+    public void Clear(byte r = 0, byte g = 0, byte b = 0) => ImgBuffer = Array.Empty<byte>();
 
-//    public void SetRGB(int sw, int sh, byte[] buffer)
-//    {
-//        Width = sw;
-//        Height = sh;
-//        ImgBuffer = buffer;
-//    }
+    public void SetRGB(int sw, int sh, byte[] buffer)
+    {
+        Width = sw;
+        Height = sh;
+        ImgBuffer = buffer;
+    }
 
-//    internal bool IsEmpty()
-//    {
-//        return ImgBuffer.Length == 0;
-//    }
-//}
-//#pragma warning restore IDE0032
-//#pragma warning restore CA1819
+    internal bool IsEmpty()
+    {
+        return ImgBuffer.Length == 0;
+    }
+}
+#pragma warning restore IDE0032
+#pragma warning restore CA1819
