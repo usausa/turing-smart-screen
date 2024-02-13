a9018db076c818705c66b563e540c8804cf29a2d
usausa <machi.pon@gmail.com>
2024-02-13 15:54:11 +0900

    Update helper

--- TuringSmartScreenLib.Helpers.SkiaSharp/Extensions.cs
+++ TuringSmartScreenLib.Helpers.SkiaSharp/Extensions.cs
@@ -2,7 +2,7 @@ namespace TuringSmartScreenLib.Helpers.SkiaSharp;
 
 using global::SkiaSharp;
 
-public static partial class Extensions
+public static class Extensions
 {
     public static IScreenBuffer CreateBufferFrom(this IScreen screen, SKBitmap bitmap)
     {
@@ -11,32 +11,24 @@ public static partial class Extensions
         return buffer;
     }
 
+    public static IScreenBuffer CreateBufferFrom(this IScreen screen, SKBitmap bitmap, int sx, int sy, int sw, int sh)
+    {
+        var buffer = screen.CreateBuffer(sw, sh);
+        buffer.ReadFrom(bitmap, sx, sy, sw, sh);
+        return buffer;
+    }
+
     public static void ReadFrom(this IScreenBuffer buffer, SKBitmap bitmap) =>
         buffer.ReadFrom(bitmap, 0, 0, bitmap.Width, bitmap.Height);
 
     public static void ReadFrom(this IScreenBuffer buffer, SKBitmap bitmap, int sx, int sy, int sw, int sh)
     {
-        if (buffer is TuringSmartScreenBufferA bufferA)
+        for (var y = 0; y < sh; y++)
         {
-            bufferA.ReadFrom(bitmap, sx, sy, sw, sh);
-        }
-        else if (buffer is TuringSmartScreenBufferB bufferB)
-        {
-            bufferB.ReadFrom(bitmap, sx, sy, sw, sh);
-        }
-        else if (buffer is TuringSmartScreenBufferC bufferC)
-        {
-            bufferC.ReadFrom(bitmap, sx, sy, sw, sh);
-        }
-        else
-        {
-            for (var y = sy; y < sy + sh; y++)
+            for (var x = 0; x < sw; x++)
             {
-                for (var x = sx; x < sx + sw; x++)
-                {
-                    var color = bitmap.GetPixel(x, y);
-                    buffer.SetPixel(x, y, color.Red, color.Green, color.Blue);
-                }
+                var color = bitmap.GetPixel(x + sx, y + sy);
+                buffer.SetPixel(x, y, color.Red, color.Green, color.Blue);
             }
         }
     }
