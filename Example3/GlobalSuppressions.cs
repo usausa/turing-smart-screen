937b91679b7d4c07ab0b80882845d07720ec0e3e
usausa <machi.pon@gmail.com>
2024-12-07 19:23:09 +0900

    Fix inspection

---
+++ Example3/GlobalSuppressions.cs
@@ -0,0 +1,2 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Do not directly await a Task", Justification = "Ignore")]
